886b5dece6fac083289578694ccf743486951d1f
nutcas3 <mauricee423@gmail.com>
2023-11-14 20:53:26 +0300

    🎉 agri-fund

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+require("hardhat-deploy");
+require("@nomiclabs/hardhat-ethers");
+
+module.exports = {
+  solidity: "0.8.1",
+  defaultNetwork: "telos_testnet",
+  namedAccounts: {
+    deployer: 0,
+  },
+  networks: {
+    telos_testnet: {
+      url: "https://testnet.telos.net/evm",
+      accounts: [process.env.TELOS_TESTNET_PRIVATE_KEY],
+      chainId: 41,
+    },
+  },
+};
