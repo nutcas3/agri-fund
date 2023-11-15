4e0db1586230fcc02d6a5fa62585f1aad9398188
nutcas3 <mauricee423@gmail.com>
2023-11-15 13:58:41 +0300

    ✅

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+import { ethers } from "hardhat";
+
+async function main() {
+
+  // We get the contract to deploy
+  const Agrifunding = await ethers.getContractFactory("Agrifunding");
+  const agrifunding = await Agrifunding.deploy();
+
+  await agrifunding.deployed();
+
+  console.log("Agrifunding deployed to:", agrifunding.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
